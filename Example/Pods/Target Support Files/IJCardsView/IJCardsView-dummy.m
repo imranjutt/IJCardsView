8bf6e0164706ebc0490ab32767385363ebe219e5
Imran Jutt <muhammad.imran.jutt@venturedive.com>
2017-07-09 19:45:43 +0500

    initail commit

---
+++ Example/Pods/Target Support Files/IJCardsView/IJCardsView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_IJCardsView : NSObject
+@end
+@implementation PodsDummy_IJCardsView
+@end
---
+++ Example/Pods/Target Support Files/Pods-IJCardsView_Example/Pods-IJCardsView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IJCardsView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_IJCardsView_Example
+@end
